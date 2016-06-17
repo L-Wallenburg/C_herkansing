d7aefaccdb124fa48514c1202ca2d37b9005fb79
L-Wallenburg <leroy.wallenburg@student.hu.nl>
2016-06-17 15:48:36 +0200

    Add files via upload

--- opdr_1_5/main.c
+++ opdr_1_5/main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
 	int i;
+	int c;
+	bool negative = false;
+	printf("Vul een character in : ");
+	c = getchar();
+	while(c == 10 || c == 43 || c == 45 && (c >=48 || c <= 57)) {
+		if(c==45){
+			negative = true;
+		}
+		c = getchar();
+	}
 	
-	
-	
-	
+	if(negative==true){
+		i = c*-1+48;
+	}
+	else{
+		i = c-48;
+	}
+
 	printf("het ingevoerde getal is %d \n", i);
 	
-	
+	getchar();
+	getchar();
 	return 0;
 }
